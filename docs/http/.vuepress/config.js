f8628c2c43c89913ebab1e58b94490afa3cfa271
karina-kasimova <casimowa.c2001@gmail.com>
2022-06-26 16:57:12 +0300

    curl file and http in request

--- docs/http/.vuepress/config.js
+++ docs/http/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Evas-Db',
+  title: 'Evas-Http',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
