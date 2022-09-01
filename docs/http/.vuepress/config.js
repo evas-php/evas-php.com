1c5e13950a8769fc7e761c27d59617db81a37f58
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-09-01 07:43:44 +0300

    http: Update config.js (upgrade to vue3)

--- docs/http/.vuepress/config.js
+++ docs/http/.vuepress/config.js
@@ -1,110 +1,65 @@
-const { description } = require('../../../package')
-const fs = require('fs');
-const path = require('path');
-module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: 'Evas-Http',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
-  description: description,
+const { ConfigBuilder } = require('../../../common/ConfigBuilder.js')
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
-  head: [
-    ['meta', { name: 'theme-color', content: '#617cbf' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
-  ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
-  themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
-    // logo: '/img/evas-php-logo.jpg',
-    nav: [
-      { text: 'Guide', link: '/guide/', },
-      { text: 'Contact',
-        items: [
-          { text: 'Vk Community', link: 'https://vk.com/evasphp' },
-          { text: 'Facebook Page', link: 'https://facebook.com/evasphp' },
-          { text: 'Telegram Chat', link: 'https://t.me/evasphp' }
-        ]
-      },
-      { text: 'GitHub', link: 'https://github.com/evas-php' },
-      { text: 'Packagist', link: 'https://packagist.org/packages/evas-php/' }
-    ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Введение', 
-          collapsable: false, // optional, defaults to true
-          sidebarDepth: 3,    // optional, defaults to 1
-          children: [
-            '/guide/',
-            '/guide/installation',
-          ]
-        },
-        {
-          title: 'Http', 
-          collapsable: false, // optional, defaults to true
-          sidebarDepth: 3,    // optional, defaults to 1
-          children: [
-            '/guide/http/request',
-            '/guide/http/response',
-            '/guide/http/uploaded-file',
-          ]
-        },
-        {
-          title: 'Curl', 
-          collapsable: false, // optional, defaults to true
-          sidebarDepth: 3,    // optional, defaults to 1
-          children: [
-            '/guide/curl/request',
-            '/guide/curl/response',
-          ]
-        },
-        {
-          title: 'Вспомогательные классы', 
-          collapsable: false, // optional, defaults to true
-          sidebarDepth: 3,    // optional, defaults to 1
-          children: [
-            '/guide/more/uri',
-            '/guide/more/cookie',
-          ]
-        },
-        {
-          title: 'Интерфейсы', 
-          collapsable: false, // optional, defaults to true
-          sidebarDepth: 3,    // optional, defaults to 1
-          children: [
-            '/guide/interfaces/request',
-            '/guide/interfaces/response',
-            '/guide/interfaces/uploaded-file',
-            '/guide/interfaces/uri',
-          ]
-        },
+const config = (new ConfigBuilder)
+.setMeta({
+  title: 'Evas-Router',
+  description: 'Official documentation of the Evas-Router php library'
+})
+.setRepo({
+  docsDir: 'docs/router'
+})
+.addNavbarRepo('evas-router')
+.setSidebar({
+  '/guide/': [
+    {
+      text: 'Введение', 
+      collapsable: false, // optional, defaults to true
+      sidebarDepth: 3,    // optional, defaults to 1
+      children: [
+        '/guide/',
+        '/guide/installation',
       ]
-    }
-  },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
-  plugins: [
-    // '@vuepress/plugin-back-to-top',
-    // '@vuepress/plugin-medium-zoom',
+    },
+    {
+      text: 'Http', 
+      collapsable: false,
+      sidebarDepth: 3,
+      children: [
+        '/guide/http/request',
+        '/guide/http/response',
+        '/guide/http/uploaded-file',
+      ],
+    },
+    {
+      text: 'Curl', 
+      collapsable: false,
+      sidebarDepth: 3,
+      children: [
+        '/guide/curl/request',
+        '/guide/curl/response',
+      ],
+    },
+    {
+      text: 'Вспомогательные классы', 
+      collapsable: false,
+      sidebarDepth: 3,
+      children: [
+        '/guide/more/uri',
+        '/guide/more/cookie',
+      ],
+    },
+    {
+      text: 'Интерфейсы', 
+      collapsable: false,
+      sidebarDepth: 3,
+      children: [
+        '/guide/interfaces/request',
+        '/guide/interfaces/response',
+        '/guide/interfaces/uploaded-file',
+        '/guide/interfaces/uri',
+      ],
+    },
   ]
-}
+}).getConfig()
+
+module.exports = config;
